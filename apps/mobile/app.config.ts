977168b97dc67bd4b31bad00a43747171cf8f8f0
Pablo Bagliere <pablo.bagliere2@gmail.com>
2022-01-05 13:30:40 -0300

    Añado variable de entorno

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,30 @@
+export default {
+  name: 'app-histrix-core-skeleton',
+  slug: 'app-histrix-core-skeleton',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    package: 'com.mundoit.histrix',
+    versionCode: 1,
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+};
